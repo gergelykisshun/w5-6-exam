70148904c06c116a32b1a7e326f262cbb00bab42
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-02-28 16:47:11 +0100

    --

--- countToArray.js
+++ countToArray.js
@@ -1,8 +1,22 @@
-const countToArray = (firstNumber, lastNumber) => {
+/* const countToArray = (firstNumber, lastNumber) => {
     let numbers = [];
     for (let i = firstNumber; i < lastNumber; i++)
         numbers.push(i.toString());
     return numbers;
+}; */
+
+
+
+/* 
+const countToArray = (firstNumber, lastNumber) => {
+    let toArray = Array(lastNumber - firstNumber);
+    const fillArray = Array.from(toArray);
+    const updateArray = fillArray.map((item, index) => `${firstNumber + index}`);
+    return updateArray;
 };
+ */
+
+const countToArray = (firstNumber, lastNumber) =>  Array.from(Array(lastNumber - firstNumber)).map((item, index) => `${firstNumber + index}`);
+
 
 console.log(countToArray(10, 30));
